24df0c850ff489a6b8f78ece4a44b25150f071c9
smulch <mulcahy.samuel@gmail.com>
2023-04-11 19:26:24 -0400

    further updating starter materials

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -4,14 +4,15 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
+import Resume from './pages/Resume';
 
 export default function PortfolioContainer() {
   const [currentPage, setCurrentPage] = useState('Home');
 
   // Add a comment describing the functionality of this method
   const renderPage = () => {
-    if (currentPage === 'Home') {
-      return <Home />;
+    if (currentPage === 'Contact') {
+      return <Contact />;
     }
     if (currentPage === 'About') {
       return <About />;
@@ -19,7 +20,10 @@ export default function PortfolioContainer() {
     if (currentPage === 'Portfolio') {
       return <Portfolio />;
     }
-    return <Contact />;
+    if (currentPage === 'Resume') {
+      return <Resume />;
+    }
+    return <Home />;
   };
 
   const handlePageChange = (page) => setCurrentPage(page);
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -5,7 +5,7 @@ export default function Resume() {
     <div>
       <h1>Blog Page</h1>
       <p>
-        This would be my resume
+        This would be my resume. Call me Ishmael. For it is I, who has been tasked with recounting the tale of a great and terrible beast, one that has captured the hearts and minds of sailors and storytellers for generations. It is a creature of immense size and power, capable of tearing apart ships and swallowing men whole. But it is more than that. It is a symbol of the unknown, a reminder of the vastness and danger of the sea. And it is this symbolism that draws men like Ahab, who seek to conquer the unconquerable, to challenge the very forces of nature. But as I sit here, pen in hand, I cannot help but wonder at the futility of such a pursuit. For what is man compared to the might of the ocean, or the power of a creature such as this? And yet, despite my doubts, I find myself drawn inexorably towards the chase, towards the very thing that may ultimately be my undoing. For such is the nature of man, and such is the power of the whale.
       </p>
     </div>
   );
